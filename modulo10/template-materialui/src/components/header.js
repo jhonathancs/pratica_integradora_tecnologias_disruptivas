0378dcd1a18503588583b3bcb79b366b5e2efadb
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2024-10-31 14:42:08 -0300

    Module 10 Exercise
    completed

--- modulo10/template-materialui/src/components/header.js
+++ modulo10/template-materialui/src/components/header.js
@@ -24,7 +24,7 @@ function Header() {
               */}
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
+              Gerenciamento de Projetos Pessoal
             </Typography>
           </Toolbar>
         </AppBar>
